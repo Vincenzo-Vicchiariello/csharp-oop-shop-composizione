368bcf94822f1f950a9d2dc77e0df6bef0ea8023
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-20 09:21:04 +0200

    Aggiungere i file di progetto.

---
+++ Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Negozio
+    {
+        private string nome;
+        private string indirizzo;
+        private string citta;
+        private int civico;
+
+
+    public Negozio(string nome, string indirizzo, string citta, int civico) { 
+
+            this.nome = nome;
+            this.indirizzo = indirizzo;
+            this.citta = citta;
+            this.civico = civico;
+        }
+
+        public void setNomeNegozio(string nome)
+        {
+            this.nome = nome;
+        }
+
+
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setIndirizzoNegozio(string indirizzo)
+        {
+            this.indirizzo = indirizzo;
+        }
+
+        public string getIndirizzo(){
+            return indirizzo;
+        }
+
+        public void setCittaNegozio(string citta)
+        {
+            this.citta = citta;
+        }
+
+
+        public string getCitta()
+        {
+            return citta;
+        }
+
+        public void setCivico(int civico)
+        {
+            this.civico = civico;
+
+        }
+      
+        public int getCivico()
+        {
+            return civico;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Shop;
+using System.Net.Http.Headers;
+
+
+Negozio mioNegozio = new Negozio("nome","indirizzo", "citta", 104);
+
+mioNegozio.setIndirizzoNegozio("Bowser");
+
+Console.WriteLine(mioNegozio.getIndirizzo());
+
+
+mioNegozio.setIndirizzoNegozio("Via Roma");
+
+Console.WriteLine(mioNegozio.getIndirizzo());
+
+
+mioNegozio.setCittaNegozio("Napoli");
+
+Console.WriteLine(mioNegozio.getCitta());
+
+
+mioNegozio.setCivico(21);
+
+Console.WriteLine(mioNegozio.getCivico());
+
